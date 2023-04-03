17aaa0637ead4c974531dd9effbda96fc61e14c9
khubaib-ali-codegenio <117726252+khubaib-ali-codegenio@users.noreply.github.com>
2023-04-03 13:09:20 +0500

    Implement Pagination button

--- src/components/pagination.jsx
+++ src/components/pagination.jsx
@@ -1,16 +1,17 @@
 import _ from 'lodash'
 
 const Pagination = props => {
-    const {itemsCount, pageSize} = props;
+    const {itemsCount, pageSize, onPageChange, currentPage} = props;
 
-    const pagesCount = itemsCount / pageSize;
+    const pagesCount = Math.ceil(itemsCount / pageSize);
+    if (pagesCount === 1) return null
     const pages = _.range(1, pagesCount + 1);
     return <nav>
         <ul className="pagination">
             {
                 pages.map(page =>
-                    <li className="page-item">
-                        <a className="page-link">{page}</a>
+                    <li className={"page-item " + ((currentPage === page) ? "active" : "")} key={page}>
+                        <a onClick={() => onPageChange(page)} className="page-link">{page}</a>
                     </li>)
             }
 
