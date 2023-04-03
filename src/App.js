f1d065e3b8acba6692fcde592a519ea191264c58
khubaib-ali-codegenio <117726252+khubaib-ali-codegenio@users.noreply.github.com>
2023-04-03 12:52:17 +0500

    Copy code from mosh

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import Movies from "./components/movies";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-        <main className="container">
-          <h1>Hello World How are you</h1>
-        </main>
+      <main className="container">
+        <Movies />
+      </main>
     );
   }
 }
